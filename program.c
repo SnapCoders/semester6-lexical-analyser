a5ff8ea8138e1bf03b07e3884e326e417ec3f36d
1garo <alevardai427@gmail.com>
2021-05-31 20:40:10 -0300

    feat: changes on file to be analysed, making if more readable

--- program.c
+++ program.c
@@ -3,12 +3,10 @@ int main()
 
     float a;
     double b;
-    ||
-    
     a = 1.1;
     int i, inc, j;
     i = 0;
-    &&
+    
     inc = 2;
     read j;
     while (i < j)
@@ -17,8 +15,11 @@ int main()
         a = a*i;
     }
 
-    if (a > 10.0)
+    if (a > 10.0 && a == 0.1)
     {
         print(a + i);
-  }
+    } else if (a < -1 || a == 0.0) 
+    {
+        print(a - i);
+    }
 }
