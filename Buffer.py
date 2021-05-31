bbaef734bc9bfc199e19231dac5984a4e701a065
1garo <alevardai427@gmail.com>
2021-05-31 20:36:57 -0300

    feat: update buffer class for more readability

--- Buffer.py
+++ Buffer.py
@@ -1,23 +1,26 @@
 class Buffer:
     def load_buffer(self):
-        arq = open('program.c', 'r')
-        text = arq.readline()
+        file = open('program.c', 'r')
+        file_buffer = file.readline()
         buffer = []
         cont = 1
 
-        # The buffer size can be changed by changing cont
-        while text != "":
-            buffer.append(text)
-            text = arq.readline()
+        while file_buffer != "":
+            buffer.append(file_buffer)
+            file_buffer = file.readline()
             cont += 1
 
-            if cont == 10 or text == '':
+            if cont == 10 or file_buffer == '':
                 # Return a full buffer
-                buf = ''.join(buffer)
+                temp_buf = ''.join(buffer)
                 cont = 1
-                yield buf
+                '''
+                When you use a function with a return value, every time you call the function, it starts with a new set of variables. 
+                In contrast, if you use a generator function instead of a normal function, the execution will start right from where it left last.
+                '''
+                yield temp_buf
 
                 # Reset the buffer
                 buffer = []
 
-        arq.close()
+        file.close()
